9f7954cbfa961c7816d0166b44e6850c38adf37c
unknown <irfanulislamfahim@gmail.com>
2022-06-25 03:06:37 +0600

    react-slick fixed

--- src/Pages/Care/Care.js
+++ src/Pages/Care/Care.js
@@ -9,13 +9,37 @@ export default class Care extends Component {
     render() {
         const settings = {
             dots: true,
+            speed: 500,
             infinite: false,
             slidesToShow: 4,
             slidesToScroll: 3,
-            // autoplay: true,
-            speed: 3000,
-            // autoplaySpeed: 2000,
-            cssEase: "linear"
+            initialSlide: 0,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        initialSlide: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
         };
         return (
             <div className="custom-bg container-fluid" style={{ paddingTop: '150px', paddingBottom: '100px' }}>
